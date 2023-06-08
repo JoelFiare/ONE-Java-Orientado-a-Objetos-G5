054670be105117d0e2b94ee7cf6a1dc51f25935c
JoelFiare <JoelFiare@gmail.com>
2023-06-07 22:05:34 -0300

    agregadi EjemploCaracteres

---
+++ java/primeros/pasos/EjemploCaracteres.java
@@ -0,0 +1,8 @@
+package java.primeros.pasos;
+
+public class EjemploCaracteres {
+	public static void main(String[] args) {
+		char caracter = 'a'; // Un solo caracter comillas simples
+		System.out.println(caracter);
+	}
+}
